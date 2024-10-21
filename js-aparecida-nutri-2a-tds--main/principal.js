710bb7e75edf75854c915acf7a178ded8b8ff1c6
giovanna280808 <giovanna.correa.souza@escola.pr.gov.br>
2024-10-21 12:32:37 -0300

    atualização_v2

--- js-aparecida-nutri-2a-tds--main/principal.js
+++ js-aparecida-nutri-2a-tds--main/principal.js
@@ -2,23 +2,27 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 var paciente = document.querySelector("#primeiro-paciente");
-
 var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-
 var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
 
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
 var tdImc = paciente.querySelector(".info-imc");
 
+var validaAltura = true;
+var validaPeso = true;
 
-if (peso <= 0) {
+if (peso <= 0 || peso >= 1000) {
     console.log("Peso inválido");
+    tdImc.textContent = "Peso inválido";
 }
 
-if (peso > 1000) {
+if (altura >= 0 || altura>= 3.0) {
     console.log("Peso inválido");
+    tdImc.textContent = "Peso inválido";
 }
 
+if (validaAltura && validaPeso) {
 var imc = peso / (altura * altura);
 tdImc.textContent = imc;
+}
